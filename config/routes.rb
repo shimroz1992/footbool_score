fa853368b6038a90a96d836fdfe830ffaad4d1b4
Shimroz <shimrozahemed.ngp@gmail.com>
2022-03-13 14:32:23 +0530

    Added routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :match_histories, only: :index
+  get 'match_histories/show_raw_file'
+  root 'match_histories#index'
 end
